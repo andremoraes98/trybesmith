e3418827973ca8707a4a4b5e0df5e7a3ee94ca1f
Andre Moraes <andre.moraes.98@gmail.com>
2022-07-29 16:30:05 -0300

    Implementação do create na camada Service

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,11 +1,16 @@
 import connection from '../models/connection';
 import UserModel from '../models/users.model';
+import User from '../interfaces/user.interface';
 
 class UserService {
   public userModel: UserModel;
 
   constructor() {
     this.userModel = new UserModel(connection);
+  }
+
+  public async create(user: User): Promise<void> {
+    await this.userModel.create(user);
   }
 }
 
