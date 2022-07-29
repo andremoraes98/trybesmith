1e1275cb36b2e53d73fe4e56b388d8f4bb57fd85
Andre Moraes <andre.moraes.98@gmail.com>
2022-07-29 17:47:19 -0300

    Configuração da camada Mordel do Order

---
+++ src/models/orders.model.ts
@@ -0,0 +1,25 @@
+import { Pool } from 'mysql2/promise';
+import Order from '../interfaces/order.interface';
+
+class OrderModel {
+  connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<Order[]> {
+    const result = await this.connection.query(
+      `SELECT
+        O.id, O.userId, P.id as productsIds
+      FROM Trybesmith.Products as P
+      INNER JOIN Trybesmith.Orders as O
+      ON P.orderId = O.id;`,
+    );
+
+    const [rows] = result;
+    return rows as Order[];
+  }
+}
+
+export default OrderModel;
