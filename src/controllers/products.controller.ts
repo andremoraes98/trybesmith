c8127451a1512fa7707baaaf64bc0b8df0a2fc54
Andre Moraes <andre.moraes.98@gmail.com>
2022-07-29 15:55:16 -0300

    Implementação do create na camada Controller

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -9,6 +9,14 @@ class ProductController {
 
     res.status(200).json(result);
   };
+
+  public create = async (req: Request, res: Response) => {
+    const { product } = req.body;
+
+    const createdProduct = this.productService.create(product);
+
+    res.status(201).json(createdProduct);
+  };
 }
 
 export default ProductController;
