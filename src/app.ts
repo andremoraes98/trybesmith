266b21310e3b8a310d930a8852f89e5afb18bf76
Andre Moraes <andre.moraes.98@gmail.com>
2022-07-29 18:08:28 -0300

    Implementação parciao do método GET /orders

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import OrderController from './controllers/orders.controller';
 import ProductController from './controllers/products.controller';
 import UserController from './controllers/users.controller';
 
@@ -8,6 +9,7 @@ app.use(express.json());
 
 const productController = new ProductController();
 const userController = new UserController();
+const orderController = new OrderController();
 
 app.get('/products', productController.getAll);
 
@@ -16,5 +18,7 @@ app.post('/products', productController.create);
 app.post('/users', userController.create);
 
 app.get('/users', userController.getAll);
+
+app.get('/orders', orderController.getAll);
 
 export default app;
