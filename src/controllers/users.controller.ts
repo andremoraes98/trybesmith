ef88f2d8ce068d008b7c13521dac594a79a6b494
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-01 15:22:55 -0300

    feat: criação do login

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import User from '../interfaces/user.interface';
+import { User, Credentials } from '../interfaces/user.interface';
 import UserService from '../services/users.service';
 
 class UserController {
@@ -18,6 +18,14 @@ class UserController {
     const result = await this.userService.getAll();
 
     return res.status(200).json(result);
+  };
+
+  public login = async (req: Request, res: Response) => {
+    const userCredential: Credentials = req.body;
+
+    const token = this.userService.generateToken(userCredential.username);
+
+    res.status(200).json({ token });
   };
 }
 
