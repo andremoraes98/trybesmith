249bd0478a0c33a68e127ba395b5b671c6eece65
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-01 14:53:17 -0300

    feat: criação da interface Credentials

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,7 +1,12 @@
-export default interface User {
+export interface User {
   id?: number,
   username: string,
   classe: string,
   level: number,
+  password: string
+}
+
+export interface Credentials {
+  username: string,
   password: string
 }
